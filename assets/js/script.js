7adbd6a0bc0ee59cb16f80f28e64e7a7373ae917
Inotila <inoati1991@gmail.com>
2021-11-18 15:49:10 +0000

    add java structure to run game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+})
+
+function rollDi(){
+
+}
+
+function keepDi(){
+
+}
+
+function endTurn(){
+
+}
+
+function score(params) {
+    
+}
+
+function addScore(params) {
+    
+}
+
+function subScore() {
+
+}
+
+function masterTurn(){
+    
+}
