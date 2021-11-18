56d527cbb545791c750ebff7c552e8afcd9f7fe3
Inotila <inoati1991@gmail.com>
2021-11-18 16:19:11 +0000

    add event listners for the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,45 @@
-document.addEventListener('DOMContentLoaded', function(){
-
+//wait for the dom to finish loading
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByClassName("play-buttons");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "play") {
+                rollDi();
+            } else if (this.getAttribute("data-type") === "keep") {
+                keepDi();
+            } else if (this.getAttribute("data-type") === "stop") {
+                endTurn();
+            } else {
+                console.log("waiting for user to play");
+            }
+        });
+    }
 })
 
-function rollDi(){
-
+function rollDi() {
+    alert("roll dice")
 }
 
-function keepDi(){
-
+function keepDi() {
+    alert("store these di")
 }
 
-function endTurn(){
-
+function endTurn() {
+    alert("end of turn")
 }
 
 function score(params) {
-    
+
 }
 
 function addScore(params) {
-    
+
 }
 
 function subScore() {
 
 }
 
-function masterTurn(){
-    
+function masterTurn() {
+
 }
